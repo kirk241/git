9a3b3cd1e28c673b7d6dbf189e407d8b53771b7d
kirk241 <92648523+kirk241@users.noreply.github.com>
2022-04-27 15:40:40 -0400

    Add files via upload

---
+++ Gamepad.java
@@ -0,0 +1,14 @@
+public class Gamepad {
+  
+  public double leftStick;
+  public double rightStick;
+
+  public boolean A;
+
+public Gamepad () {
+  this.leftStick = 0;
+  this.rightStick = 0;
+
+  this.A = false;
+  }
+}
